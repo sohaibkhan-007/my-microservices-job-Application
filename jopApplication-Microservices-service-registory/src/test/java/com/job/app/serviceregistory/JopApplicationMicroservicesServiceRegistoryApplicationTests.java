2796014bac62b6e76538e84cce6ecc922dcac0cd
sohaibkhan-007 <sohaibkhanatwork@gmail.com>
2024-07-25 18:25:28 +0530

    added service-registory

---
+++ jopApplication-Microservices-service-registory/src/test/java/com/job/app/serviceregistory/JopApplicationMicroservicesServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.app.serviceregistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopApplicationMicroservicesServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
