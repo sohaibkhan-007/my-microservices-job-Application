5a7d62577f40b4ebee86097a7a84aac42e88a767
sohaibkhan-007 <sohaibkhanatwork@gmail.com>
2024-08-07 18:14:14 +0530

    pushing changes in JobServiceImpl

--- jopApplication-Microservices-job/src/main/java/com/job/app/job/impl/JobServiceImpl.java
+++ jopApplication-Microservices-job/src/main/java/com/job/app/job/impl/JobServiceImpl.java
@@ -21,6 +21,8 @@ import com.job.app.job.JobService;
 import com.job.app.mapper.JobMapper;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -31,6 +33,7 @@ public class JobServiceImpl implements JobService {
 	JobRepository jobRepository;
 	private CompanyClient companyClient;
 	private ReviewClient reviewClient;
+	private int attempts = 0;
 
 	public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
 		this.jobRepository = jobRepository;
@@ -39,8 +42,11 @@ public class JobServiceImpl implements JobService {
 	}
 
 	@Override
-	@CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//	@CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//	@Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+	@RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
 	public List<JobDto> findAll() {
+		System.out.println("attempts " + ++attempts);
 		List<Job> jobs = jobRepository.findAll();
 		return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
 	}
